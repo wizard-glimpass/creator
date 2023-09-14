2e0fd12fef5c1ace3c76fb43109d1da7fc6d6c40
Manish bharti <parasparas0001@gmail.com>
2023-09-14 22:23:20 +0530

    changes.

--- src/component/Compass.js
+++ src/component/Compass.js
@@ -28,30 +28,38 @@ function Compass(props) {
   };
   ///////////////////////
 
-  const [dy, setdy] = useState(0);
-
   const dirRef = useRef({ alpha: 0, beta: 0, gamma: 0 });
   const accRef = useRef();
   const distRef = useRef(0);
 
   //changes for speed
 
+  //changes for speed
+  const final_s = useRef(0);
   const prev_force = useRef(0);
   const final_force = useRef(0);
-
   const prev_time = useRef(Date.now());
-
+  const sp_x = useRef(0);
   const final = useRef(0);
   const push = useRef(0);
   const steps = useRef(0);
   const push_y = useRef(0);
   const travel = useRef(0);
   const travel_state = useRef(0);
-
   const omega_a = useRef(0);
   const omega_a_p = useRef(0);
   const omega_max_p = useRef(0);
   const omega_max = useRef(0);
+  const lrav_prev = useRef(-1);
+  const lrav_now = useRef(0);
+  const lrav_push = useRef(0);
+  const lrav_final = useRef(0);
+  const lrah_final = useRef(0);
+  const lrov_final = useRef(0);
+  const lroh_prev = useRef(-1);
+  const lroh_now = useRef(0);
+  const lroh_push = useRef(0);
+  const lroh_final = useRef(0);
 
   //////////////////////
   const [alpha, setAlpha] = useState(0);
@@ -61,6 +69,14 @@ function Compass(props) {
   const [isWalking, setIsWalking] = useState(true);
   const [alphaSum, setAlphaSum] = useState(0);
   const [alphaReadingsCounted, setAlphaReadingsCounted] = useState(0);
+
+  const [X, setX] = useState(0);
+  const [Y, setY] = useState(0);
+  const [Z, setZ] = useState(0);
+  const [dx, setdx] = useState(0);
+  const [dy, setdy] = useState(0);
+  const [directionData, setDirectionData] = useState({});
+  const [final_speed, setFinalSpeed] = useState(0);
 
   const sendAngleStepsPayload = () => {
     const avgAlpha = alphaSum / alphaReadingsCounted;
@@ -91,6 +107,7 @@ function Compass(props) {
 
     const timeInterval = 0;
     distRef.current += parseInt(event.acceleration.x) * timeInterval;
+    setDirectionData(dirRef.current);
 
     const acc_th = 0.1;
     const time_th = 0.3;
@@ -108,16 +125,22 @@ function Compass(props) {
     if (Math.abs(accn_z) < acc_th) {
       accn_z = 0;
     }
-
     const sin_a = parseInt(dirRef.current.alpha); // * (Math.PI / 180))
     const sin_b = parseInt(dirRef.current.beta); // * (Math.PI / 180))
     const sin_g = parseInt(dirRef.current.gamma); // * (Math.PI / 180))
     const rate_a = parseInt(event.rotationRate.alpha);
-    const rate_c = parseInt(event.rotationRate.gamma);
+    const rate_b = parseInt(event.rotationRate.beta);
+    let rate_c = parseInt(event.rotationRate.gamma);
+    const final_omega = inertialFrame(
+      sin_a * (Math.PI / 180),
+      sin_b * (Math.PI / 180),
+      sin_g * (Math.PI / 180),
+      rate_a,
+      rate_b,
+      rate_c
+    );
+    rate_c = final_omega[2];
 
-    if (accn_y < 0) {
-      final_force.current += Math.abs(accn_y);
-    }
     //windows.alert(sin_b)
     final.current = inertialFrame(
       sin_a * (Math.PI / 180),
@@ -128,17 +151,51 @@ function Compass(props) {
       accn_z
     );
 
+    // accn vertical
+    if (lrav_prev.current == -1) {
+      if (final.current[2] < 0) {
+        lrav_push.current -= 1;
+      }
+      if (lrav_push.current < -10) {
+        lrav_now.current = 1;
+      }
+    } else {
+      if (final.current[2] > 0) {
+        lrav_push.current += 1;
+      }
+      if (lrav_push.current > 3) {
+        lrav_now.current = -1;
+      }
+    }
+    lrav_final.current = lrav_prev.current * lrav_now.current;
+
+    // omega horizontal
+    if (lroh_prev.current == -1) {
+      if (rate_c < 0) {
+        lroh_push.current -= 1;
+      }
+      if (lroh_push.current < -10) {
+        lroh_now.current = 1;
+      }
+    } else {
+      if (rate_c > 0) {
+        lroh_push.current += 1;
+      }
+      if (lroh_push.current > 10) {
+        lroh_now.current = -1;
+      }
+    }
+    lroh_final.current = lroh_prev.current * lroh_now.current;
+
     //push implementation
     const timeDiff = (Date.now() - prev_time.current) / 1000;
     if (final.current[2] > 0) {
       if (push.current < 1) {
         push.current += 0.334;
       }
-
       if (accn_y < 0 && push_y.current < 1) {
         push_y.current += 0.334;
       }
-
       if (push_y.current >= 1 && timeDiff > time_th) {
         push_y.current = 1;
         travel.current += 1;
@@ -157,12 +214,18 @@ function Compass(props) {
         (push_y.current >= 1 || travel_state.current == 1)
       ) {
         if (
-          (omega_max.current < 50 && omega_max.current > 10) ||
-          travel_state.current == 0
+          omega_max.current < 50 &&
+          omega_max.current > 5 &&
+          (lroh_final.current == -1 || lrav_final.current == -1)
+          //||travel_state.current == 0
         ) {
           steps.current += 1;
           push.current = 1;
           prev_time.current = Date.now();
+          lroh_prev.current = lroh_now.current;
+          lroh_push.current = 0;
+          lrav_prev.current = lrav_now.current;
+          lrav_push.current = 0;
 
           if (omega_a.current > 0) {
             omega_a_p.current = omega_a.current;
@@ -178,12 +241,15 @@ function Compass(props) {
             prev_force.current = final_force.current;
           }
           final_force.current = 0;
-        } else {
-          if (omega_max.current > 0) {
-            omega_max_p.current = omega_max.current;
-          }
-          omega_max.current = 0;
+          //   window.angleError = Math.atan(Math.sin(sin_a* (Math.PI / 180))/(window.currentStep-Math.cos(sin_a* (Math.PI / 180))));
+          //   window.stepError = Math.sqrt((window.currentStep - Math.cos(sin_a* (Math.PI / 180))) + Math.sin(sin_a* (Math.PI / 180)));
+          //   window.stepError = window.currentStep - window.stepError-1;
         }
+
+        if (omega_max.current > 0) {
+          omega_max_p.current = omega_max.current;
+        }
+        omega_max.current = 0;
       }
     }
 
@@ -195,11 +261,14 @@ function Compass(props) {
       }
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // left-right implementation
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+    sp_x.current = Math.sqrt(
+      prev_force.current * omega_a_p.current * omega_max_p.current
+    );
+    setFinalSpeed(final_s.current.toFixed(3));
+    setX(parseFloat(lrah_final.current).toFixed(4));
+    setY(parseFloat(lrov_final.current).toFixed(4));
+    setZ(parseFloat(lroh_final.current).toFixed(4));
+    setdx(parseFloat(sp_x.current).toFixed(4));
     setdy(parseFloat(steps.current));
   };
   const handleOrientation = (event) => {
@@ -257,12 +326,12 @@ function Compass(props) {
   const addCheckpoint = () => {
     const avgAlpha = alphaSum / alphaReadingsCounted;
     props.addCheckpoint(dy, avgAlpha);
+    steps.current = 0;
     setdy(0);
     setAlphaSum(0);
     setAlphaReadingsCounted(1);
   };
 
-  console.log(calcStepsValue);
   return (
     <>
       {confirmAngleAndSteps && (
